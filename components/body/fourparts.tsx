3acf98fb3925dd10c90042810ef4a084d80393cd
umar-developer-01 <umarkhandev9@gmail.com>
2025-05-18 17:56:50 +0530

    Changes in the four parts

--- components/body/fourparts.tsx
+++ components/body/fourparts.tsx
@@ -40,7 +40,7 @@ const info = [
 const FourParts = () => {
     return (
         <>
-            <div className="grid grid-cols-4 px-54 pt-8">
+            <div className="grid grid-cols-4 px-54 pt-8 mb-20">
                 {info.map((data, index) => (
                     <Card
                         key={index}
@@ -73,7 +73,7 @@ type CardProps = {
 
 const Card = ({ image, tag, desc, name, plays, time }: CardProps) => {
     return (
-        <div className="flex flex-col border rounded-md px-6">
+        <div className="flex flex-col border-r border-gray-200 px-6">
             <Image
                 src={image}
                 alt="TED Logo"
@@ -85,7 +85,7 @@ const Card = ({ image, tag, desc, name, plays, time }: CardProps) => {
             <div className="text-red-600 font-semibold mb-4">{tag}</div>
 
             <div className="text-xl font-bold mb-2">{desc}</div>
-            <div className="flex item-center gap-2 text-gray-500">
+            <div className="flex items-center gap-2 text-gray-500 whitespace-nowrap">
                <div>{name}</div>
                <div className="h-1 w-1 rounded-full bg-gray-500"></div>
                <div>{plays}</div>
