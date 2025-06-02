8dbc4a90899de5d5169408b21ecdd25553238111
umar-developer-01 <umarkhandev9@gmail.com>
2025-06-03 00:15:16 +0530

    added tredning -ted

--- components/trendingTed.tsx
+++ components/trendingTed.tsx
@@ -3,7 +3,135 @@
 const TrendingTed = () => {
     return (
         <>
-            Trending Tedx
+            <div className="mx-60 mt-2  border-t  border-gray-500 mb-16">
+
+                <p className="font-semibold text-lg pt-4 pb-4">TRENDING TEDX IDEAS</p>
+
+                <div className="grid grid-rows-3 grid-cols-2 gap-x-2 gap-y-6">
+                    <div className="bg-gray-100 h-[160px] w-[700px] flex items-center ">
+                        <div className="text-6xl px-12">
+                            1
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <div className="text-red-600 font-semibold">SCIENCE</div>
+                            <div className="text-3xl font-medium leading-8">
+                                How to make your cat happier --- in 3 minutes
+                            </div>
+                            <div className="flex items-center text-gray-500 gap-2 text-md">
+                                <div>
+                                    Nicky Trevorrow
+                                </div>
+                                <div className="h-1 w-1 rounded-full bg-gray-500"></div>
+                                <div>
+                                    8 months ago
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-gray-100 h-[160px] w-[700px] flex items-center ">
+                        <div className="text-6xl px-12">
+                            2
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <div className="text-red-600 font-semibold">SCIENCE</div>
+                            <div className="text-3xl font-medium leading-8">
+                                How to make your cat happier --- in 3 minutes
+                            </div>
+                            <div className="flex items-center text-gray-500 gap-2 text-md">
+                                <div>
+                                    Nicky Trevorrow
+                                </div>
+                                <div className="h-1 w-1 rounded-full bg-gray-500"></div>
+                                <div>
+                                    8 months ago
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-gray-100 h-[160px] w-[700px] flex items-center ">
+                        <div className="text-6xl px-12">
+                            3
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <div className="text-red-600 font-semibold">SCIENCE</div>
+                            <div className="text-3xl font-medium leading-8">
+                                How to make your cat happier --- in 3 minutes
+                            </div>
+                            <div className="flex items-center text-gray-500 gap-2 text-md">
+                                <div>
+                                    Nicky Trevorrow
+                                </div>
+                                <div className="h-1 w-1 rounded-full bg-gray-500"></div>
+                                <div>
+                                    8 months ago
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-gray-100 h-[160px] w-[700px] flex items-center ">
+                        <div className="text-6xl px-12">
+                            4
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <div className="text-red-600 font-semibold">SCIENCE</div>
+                            <div className="text-3xl font-medium leading-8">
+                                How to make your cat happier --- in 3 minutes
+                            </div>
+                            <div className="flex items-center text-gray-500 gap-2 text-md">
+                                <div>
+                                    Nicky Trevorrow
+                                </div>
+                                <div className="h-1 w-1 rounded-full bg-gray-500"></div>
+                                <div>
+                                    8 months ago
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="bg-gray-100 h-[160px] w-[700px] flex items-center ">
+                        <div className="text-6xl px-12">
+                            5
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <div className="text-red-600 font-semibold">SCIENCE</div>
+                            <div className="text-3xl font-medium leading-8">
+                                How to make your cat happier --- in 3 minutes
+                            </div>
+                            <div className="flex items-center text-gray-500 gap-2 text-md">
+                                <div>
+                                    Nicky Trevorrow
+                                </div>
+                                <div className="h-1 w-1 rounded-full bg-gray-500"></div>
+                                <div>
+                                    8 months ago
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                   
+                    <div className="bg-gray-100 h-[160px] w-[700px] flex items-center ">
+                        <div className="text-6xl px-12">
+                            6
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <div className="text-red-600 font-semibold">SCIENCE</div>
+                            <div className="text-3xl font-medium leading-8">
+                                How to make your cat happier --- in 3 minutes
+                            </div>
+                            <div className="flex items-center text-gray-500 gap-2 text-md">
+                                <div>
+                                    Nicky Trevorrow
+                                </div>
+                                <div className="h-1 w-1 rounded-full bg-gray-500"></div>
+                                <div>
+                                    8 months ago
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
